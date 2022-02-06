cfc27fb9aa41dd8fcc61d39458bef89c3e3f7fc5
Kaide <kaidewu@protonmail.com>
2022-02-06 16:52:56 +0100

    Update my dotfiles

--- bin/ethernet_status.sh
+++ bin/ethernet_status.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
  
-#echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig enp39s0 | grep "inet " | awk '{print $2}')%{u-}"
+#ip route get 1 | awk "{print $7}"
+echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig enp39s0 | grep "inet " | awk '{print $2}')%{u-}"
+
 #echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig enp0s3 | grep "inet " | awk '{print $2}')%{u-}"
-echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig eno1 | grep "inet " | awk '{print $2}')%{u-}" 
+#echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig eno1 | grep "inet " | awk '{print $2}')%{u-}" 
